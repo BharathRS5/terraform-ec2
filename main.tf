c2268b54e7caa6d957d5c395a6fd40fd057008cf
RSB <rsbharathrsb@gmail.com>
2023-09-29 15:41:31 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 # module "ec2" {
-#   source = "../terraform-module"
+#   source = "../terraform-module-ec2"
 #   ami_id = "ami-03265a0778a880afb"
 #   instance_type = "t2.micro"
 # }
 
 module "ec2" {
-  source = "../terraform-module"
+  source = "../terraform-module-ec2"
   ami_id = var.ami_id
   instance_type = var.instance_type
 }
